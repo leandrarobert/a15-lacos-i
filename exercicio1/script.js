b97c6e7df479213f88ddb0adb378da9c94f6b965
leandrarobert <leandraroberta234@gmail.com>
2022-09-12 21:37:39 -0300

    atividade feita

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+let coxinhas = prompt("Você deseja comer mais coxinhas/ responda /s para sim/ n para não")
+let preco = 2.50
+let conta;
+
+if(coxinhas === "s") {
+    conta += preco
+    console.log ("sua conta é," + conta + "reais")
+}
